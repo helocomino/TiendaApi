a5d93334f16ec95b3ad6e0f7c8dc83e735bd4f4d
Joshua Benali Sejas Baya <joshuabsejasbaya@gmail.com>
2026-06-24 13:09:45 -0400

    Fix: Cambiar base de datos manual a Render PostgreSQL

--- Program.cs
+++ Program.cs
@@ -3,11 +3,9 @@ using TiendaApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Configurar Conexión a PostgreSQL
-// REEMPLAZA TU BLOQUE ANTERIOR POR ESTE:
-// 1. Pega aquí adentro tu enlace original copiado de Supabase
-// 1. Pega aquí tu enlace original directo de Supabase
-var connectionUri = new Uri("postgresql://postgres.mpqrhfgvvvqualwuscoa:1GustavotkGuss@db.mpqrhfgvvvqualwuscoa.supabase.co:5432/postgres");
+// 1. Configurar Conexión a PostgreSQL con la base de datos de Render
+// PEGA AQUÍ TU EXTERNAL DATABASE URL DE RENDER DIRECTAMENTE:
+var connectionUri = new Uri("postgresql://tienda_db_0pv7_user:q7DYCgyAUtmIO2e1YFFHoDIaQqhQ6OfY@dpg-d8u0m7vlk1mc73c665k0-a.oregon-postgres.render.com/tienda_db_0pv7");
 
 // 2. Extraemos los componentes automáticamente para armar una cadena limpia
 var dbUser = connectionUri.UserInfo.Split(':')[0];
@@ -21,6 +19,7 @@ var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username=
 // 3. Conectamos Entity Framework con la cadena formateada
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
+
 // 2. Habilitar CORS para conectar con el Frontend
 builder.Services.AddCors(options =>
 {
@@ -31,15 +30,13 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // Puedes dejar esta línea, no da problemas
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// SE ELIMINÓ EL BLOQUE DE SWAGGER QUE ESTABA AQUÍ
-
 app.UseCors("PermitirTodo");
 app.UseAuthorization();
-app.UseStaticFiles(); // <-- Agrega esto para que sirva el archivo index.html
+app.UseStaticFiles(); 
 app.MapControllers();
 
 app.Run();
